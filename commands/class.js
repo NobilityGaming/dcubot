6e19b0af849e059fc4d4729cbad449edab88b671
Hamzah Zahoor <lordhammyrblx@gmail.com>
2019-10-11 11:19:53 +0100

    Attempted to fix puppeteer failing to launch browser

--- commands/class.js
+++ commands/class.js
@@ -48,7 +48,13 @@ console.log("Day: " + day)
 
       message.channel.send(`Getting Class Info...`);
 // attempt to get class info
-      const browser = await puppeteer.launch({ headless: true });
+      const browser = await puppeteer.launch({
+        headless: true,
+        args : [
+          '--no-sandbox',
+          '--disable-setuid-sandbox'
+        ]
+      });
       const page = await browser.newPage();
       await page.setViewport({ width: 1920, height: 1080 });
       await page.goto(itemUrl, { waitUntil: 'networkidle2'});
